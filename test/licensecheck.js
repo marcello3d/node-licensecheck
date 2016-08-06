7d402c2d89cb8ab79d548c133ea1cb6478ff29a2
Marcello Bastéa-Forte <marcello@marcello-home.local>
2016-08-06 10:42:50 -0700

    strip licenseFile from tests (for now)

--- test/licensecheck.js
+++ test/licensecheck.js
@@ -7,23 +7,30 @@ var licensecheck = require('../index.js')
 
 suite('licensecheck')
 
+function replacer (key, value) {
+  return key === 'licenseFile' ? undefined : value
+}
+function removeLicenseFileKeys (object) {
+  return JSON.parse(JSON.stringify(object, replacer))
+}
+
 test('licensecheck self', function () {
   assert.deepEqual(
-    licensecheck('.', path.resolve(__dirname, '..')),
-    require('./self.json')
+    removeLicenseFileKeys(licensecheck('.', path.resolve(__dirname, '..'))),
+    removeLicenseFileKeys(require('./self.json'))
   )
 })
 
 test('licensecheck --dev', function () {
   assert.deepEqual(
-    licensecheck('.', path.resolve(__dirname, '..'), null, true),
-    require('./self-dev.json')
+    removeLicenseFileKeys(licensecheck('.', path.resolve(__dirname, '..'), null, true)),
+    removeLicenseFileKeys(require('./self-dev.json'))
   )
 })
 
 test('licensecheck mochajs', function () {
   assert.deepEqual(
-    licensecheck('.', path.resolve(__dirname, '../node_modules/mocha')),
-    require('./mocha.json')
+    removeLicenseFileKeys(licensecheck('.', path.resolve(__dirname, '../node_modules/mocha'))),
+    removeLicenseFileKeys(require('./mocha.json'))
   )
 })
